7143b7fdf60a9f42cfbc5a82ef12609d344528de
Dominic Minnich <dominicminnich@gmail.com>
2024-10-16 11:19:05 -0400

    eye redesigned

--- app/routes.py
+++ app/routes.py
@@ -16,13 +16,19 @@ def load_user(user_id):
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Registration successful! Please log in.', 'success')
-        return redirect(url_for('main.login'))
+        user = User.query.filter_by(username=form.username.data).first()
+        
+        if user:
+            flash('Username already exists. Please choose a different username.', 'danger')
+        else:
+            new_user = User(username=form.username.data)
+            new_user.set_password(form.password.data)
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registration successful! Please log in.', 'success')
+            return redirect(url_for('main.login'))
     return render_template('register.html', form=form)
+
 
 @main.route('/login', methods=['GET', 'POST'])
 def login():
@@ -30,12 +36,12 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None:
-            flash('Username not found', 'danger')
+            flash('- Username Not Found -', 'danger')
         elif not user.check_password(form.password.data):
-            flash('Incorrect password', 'danger')
+            flash('- Incorrect Password -', 'danger')
         else:
             login_user(user)
-            flash('Logged in successfully!', 'success')
+            flash('Logged In Successfully!', 'success')
             return redirect(url_for('main.home'))
     return render_template('login.html', form=form)
 
