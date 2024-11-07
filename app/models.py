58b5f8dd36b70757b87498279bd615361ecd9188
benichkyle <112586285+benichkyle@users.noreply.github.com>
2024-11-06 19:58:53 -0500

    Updated models.py to further fit TL's vision.

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin  # Import UserMixin for built-in methods
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -30,12 +31,13 @@ class User(db.Model, UserMixin):  # Inherit from UserMixin to add necessary prop
     
 #Kyle Benich
 class Project(db.Model):
-    id = db.Column(db.Integer, primary_key=True) # Primary key, unique identifier for each project
+    id = db.Column(db.Integer, primary_key=True) # Primary key, unique identifier for each project (This is what a user has in their "projects" list)
     link = db.Column(db.String(255), unique=True, nullable=False) # Unique link to Figma
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) # Time of creation, used for EoL
-    username = db.Column(db.String(150), nullable=False) # Username of creator
-    visibility = db.Column(db.String(50), nullable=False) # Public or private, could do bool but idk which would default true
-    tasks = db.Column(db.JSON, nullable=False) # JSON object of tasks (could swap to array, but json is more flexible)
+    creator = db.Column(db.Integer, nullable=False) # User ID of creator
+    priviledged = db.Column(db.Boolean, nullable=False) # This will not change, and allows the collaberator field to be used. This allows for demotion of a owner to not affect the project
+    tasks = db.Column(db.JSON, nullable=False) # JSON object of tasks (Example in Disc)
     max_submissions = db.Column(db.Integer, nullable=False) # Max number of submissions
     eol_time = db.Column(db.DateTime, nullable=False) # End of life time
-    collaborators = db.Column(db.JSON, nullable=False) # JSON object of collaborators (could swap to array, but json is more flexible)
+    collaborators = db.Column(db.JSON, nullable=False) # JSON object of collaborators (Example in Disc)
+    numPauses = db.Column(db.Integer, nullable=False) # Number of times the project has been paused (Maximum of.. 3? 2?)
