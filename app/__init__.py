8778ad3fc781b7fae42b145063139575b28badb5
Dominic Minnich <dominicminnich@gmail.com>
2024-11-11 13:40:34 -0500

    404 error page created and working

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 # Dominic Minnich 2024
 # __init__.py, initialization file for the Flask application
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from .models import db
@@ -16,6 +16,10 @@ def create_app():
     login_manager.login_view = 'main.login'  # Redirect unauthorized users to login page
 
     app.register_blueprint(main)
+    
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
 
     with app.app_context():
         db.create_all()  # Creates database tables for our data models
